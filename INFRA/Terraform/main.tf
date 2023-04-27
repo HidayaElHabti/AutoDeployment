90949771c1f1a91cd07035bc649b5ea922fc62ee
HidayaElHabti <hadouya24@gmail.com>
2023-04-27 14:53:13 +0000

    Adding security rule to allow incoming traffic on 8081 for nexus

--- INFRA/Terraform/main.tf
+++ INFRA/Terraform/main.tf
@@ -48,6 +48,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  
+  security_rule {
+    name                       = "nexus"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8081"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
