8b9422f7a589396c175a693b46839ba119e0e0ab
HidayaElHabti <hadouya24@gmail.com>
2023-04-27 15:05:49 +0000

    Change public ip allocation method to Static

--- INFRA/Terraform/main.tf
+++ INFRA/Terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Create Network Security Group and rule
